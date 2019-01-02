82397ab50a09346b12881f6c2d9a7d011a1fd966
chrycko9491@gmail.com <chrycko9491@gmail.com>
2019-01-02 09:51:09 -0500

    made main blog class.

--- blog/database/migrations/2019_01_02_144335_create_blogs_table.php
+++ blog/database/migrations/2019_01_02_144335_create_blogs_table.php
@@ -15,6 +15,9 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
